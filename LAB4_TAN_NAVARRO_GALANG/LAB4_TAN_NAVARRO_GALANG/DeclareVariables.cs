6de3f37c83caba7be96647786f0c33ac878ac439
aleomarckus <33966499+aleomarckus@users.noreply.github.com>
2018-10-15 15:40:57 +0800

    Add files via upload

---
+++ LAB4_TAN_NAVARRO_GALANG/LAB4_TAN_NAVARRO_GALANG/DeclareVariables.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB4_TAN_NAVARRO_GALANG
+{
+    class DeclareVariables
+    {
+
+        public static Double num1 = 2;
+        public static Double num2 = 4; 
+        public static Double sum, diff, mul, div;
+
+        public Double summ()
+        {
+            sum = num1 + num2;
+            return sum;
+
+        }
+        public Double difff()
+        {
+            diff = num1 - num2;
+            return diff;
+        }
+
+        public Double mull()
+        {
+            mul = num1 * num2;
+            return DeclareVariables.mul;
+        }
+        public Double divv()
+        {
+            div = num1 / num2;
+            return div;
+        }
+
+    }
+}
---
+++ LAB4_TAN_NAVARRO_GALANG/LAB4_TAN_NAVARRO_GALANG/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB4_TAN_NAVARRO_GALANG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double fin_sum, fin_diff, fin_mul, fin_div;
+            DeclareVariables decvar = new DeclareVariables();
+       
+
+            fin_sum = decvar.summ();
+            fin_diff= decvar.difff();
+            fin_mul = decvar.mull();
+            fin_div = decvar.divv();
+
+            Console.WriteLine("The sum of " + DeclareVariables.num1 + " And " + DeclareVariables.num2 + " is " + fin_sum);
+            Console.WriteLine("The difference of " + DeclareVariables.num1 + " And " + DeclareVariables.num2 + " is " + fin_diff);
+            Console.WriteLine("The product of " + DeclareVariables.num1 + " And " + DeclareVariables.num2 + " is " + fin_mul);
+            Console.WriteLine("The quotient of " + DeclareVariables.num1 + " And " + DeclareVariables.num2 + " is " + fin_div);
+
+            Console.ReadKey();
+
+        }
+    }
+}
